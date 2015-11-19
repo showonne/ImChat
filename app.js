f19d95f22f52b5ac673a29ee777fc956e53f70e9
showonne <918677896@qq.com>
2015-11-19 16:29:01 +0800

    update

--- app.js
+++ app.js
@@ -77,8 +77,9 @@ app.use(function(err, req, res, next) {
 var server = require('http').createServer(app);
 var io = require('socket.io')(server);
 
-server.listen(3000, function(){
-  console.log('server is listening on port 3000');
+
+server.listen(80, function(){
+  console.log('server is listening on port 80');
 });
 
 var socketArr = {};
